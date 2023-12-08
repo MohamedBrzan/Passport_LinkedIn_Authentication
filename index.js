7c843ead25ab67c7231f9069b4acdd14c99289c4
Mohamed Brzan <mohamedbrzan11@gmail.com>
2023-12-08 03:00:31 +0200

    Fix Link

--- index.js
+++ index.js
@@ -17,7 +17,16 @@ passport.use(
       scope: ['r_emailaddress', 'r_liteprofile'],
       state: true,
     },
-    async (accessToken, refreshToken, profile, done) => done(null, profile)
+    function (accessToken, refreshToken, profile, done) {
+      // asynchronous verification, for effect...
+      process.nextTick(function () {
+        // To keep the example simple, the user's LinkedIn profile is returned to
+        // represent the logged-in user. In a typical application, you would want
+        // to associate the LinkedIn account with a user record in your database,
+        // and return that user instead.
+        return done(null, profile);
+      });
+    }
   )
 );
 
